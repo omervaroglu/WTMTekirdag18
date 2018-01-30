fc9bca7ad34e53d851a066ce08158975022025f7
Ömer Varoğlu <omervaroglu@gmail.com>
2018-01-31 00:46:53 +0300

    abc

---
+++ src/Components/Map.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { View, Button } from 'react-native';
+import getDirections from 'react-native-google-maps-directions';
+
+export default class gmapsDirections extends Component {
+
+  handleGetDirections = () => {
+    const data = {
+       source: {
+        latitude: -33.8356372,
+        longitude: 18.6947617
+      },
+      destination: {
+        latitude: -33.8600024,
+        longitude: 18.697459
+      },
+      params: [
+        {
+          key: 'dirflg',
+          value: 'w'
+        }
+      ]
+    };
+
+    getDirections(data);
+  }
+
+  render() {
+    return (
+      <View>
+        <Button onPress={this.handleGetDirections} title="Get Directions" />
+      </View>
+    );
+  }
+}
---
+++ src/common/SideButton.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+
+const SideButton = ({ onPress, children }) => {
+  const { buttonStyle, textStyle } = styles;
+  return (
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+    <Text style={textStyle}> {children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+   textStyle: {
+     alignSelf: 'center',
+     justifyContent: 'center',
+     color: '#ffffff',
+     fontSize: 20,
+     fontWeight: '100',
+     height: 'auto'
+      },
+buttonStyle: {
+    backgroundColor: '#019879',
+    borderRadius: 25,
+    width: 210,
+    height: 40,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    borderColor: '#1be9b6',
+    marginTop: 10,
+  },
+};
+
+export default SideButton;
